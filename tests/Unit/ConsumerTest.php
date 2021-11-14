010577845f707789048decfd200183557e8b587e
FrederST <freder.hernandez@correounivalle.edu.co>
2021-11-13 21:00:19 -0500

    rabbit tests

--- tests/Unit/ConsumerTest.php
+++ tests/Unit/ConsumerTest.php
@@ -1,41 +1,30 @@
 <?php
 
-namespace Tests\Unit;
+namespace E4\Messaging\Test\Unit;
 
-use E4\Messaging\Consumer;
-use PhpAmqpLib\Channel\AMQPChannel;
+use E4\Messaging\AMQPConnection;
+use E4\Messaging\Facades\Messaging;
+use PhpAmqpLib\Exchange\AMQPExchangeType;
+use PhpAmqpLib\Message\AMQPMessage;
 use Tests\TestCase;
 
 class ConsumerTest extends TestCase
 {
-    public function test_it_can_construct_a_consumer(): void
+    private string $exchange = 'direct_exchange';
+    private string $queue = 'queue';
+
+    public function test_consume_message()
     {
-        $consumer = new Consumer('queueName', $this->prepareAMQPChannel());
-
-        $this->assertNotNull($consumer);
-    }
-
-    public function test_it_can_set_queue_name(): void
-    {
-        $consumer = new Consumer('queueName', $this->prepareAMQPChannel());
-        $consumer->setQueue('queue');
-
-        $this->assertEquals('queue', $consumer->getQueue());
-    }
-
-    public function test_it_can_consume(): void
-    {
-        $consumer = new Consumer('queueName', $this->prepareAMQPChannel());
-        $consumer->setQueue('queue');
-        $consumer->consume(function () {
+        $connection = new AMQPConnection(config('messagingapp.connections.rabbitmq'));
+        $channel = $connection->getChannel();
+        $channel->queue_declare($this->queue, false, true, false, false);
+        $channel->exchange_declare($this->exchange, AMQPExchangeType::DIRECT, false, true, false);
+        $channel->queue_bind($this->queue, $this->exchange);
+        $message = new AMQPMessage('data1');
+        $channel->basic_publish($message, $this->exchange);
+        Messaging::consume(function (AMQPMessage $message) {
+            $this->assertEquals($message->body, 'data1');
+            $message->ack();
         });
-        $this->assertTrue(true);
-    }
-
-    protected function prepareAMQPChannel()
-    {
-        return $this->getMockBuilder(AMQPChannel::class)
-            ->disableOriginalConstructor()
-            ->getMock();
     }
 }
